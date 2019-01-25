46762d5416d8f9b809fa0c5ae46be1656cb1d415
Amulya Metku <devi.amulya72@gmail.com>
2019-01-25 10:45:27 +0530

    Displayed json on page

--- src/components/App/foodmenu/index.js
+++ src/components/App/foodmenu/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 
+
+
+//Running ok
 class Foodmenu extends React.Component{
     constructor()
   {
@@ -13,15 +16,15 @@ class Foodmenu extends React.Component{
 
   componentDidMount()
   {
-    <h1> JSON fetching </h1>
-    const url = "http://localhost:9000/foods"; 
+
+    const url = "http://10.10.200.25:9000/foods"; 
     
     let headers = new Headers();
 
     headers.append('Content-Type', 'application/json');
     headers.append('Accept', 'application/json');
 
-    headers.append('Access-Control-Allow-Origin', 'http://localhost:9000/foods');
+    headers.append('Access-Control-Allow-Origin', 'http://10.10.200.25:9000/foods');
     headers.append('Access-Control-Allow-Credentials', 'true');
 
     headers.append('GET', 'POST');
@@ -31,9 +34,9 @@ class Foodmenu extends React.Component{
         method: 'GET'
     })
     .then(response => response.json())
-    .then(contents => {console.log(contents);
+    .then(contents => {console.log("in fetch: "+ contents);
                         this.setState ({
-                        data : contents.data})
+                        data : contents})
          })
     .catch(() => console.log("Can’t access " + url + " response. "))
    
@@ -41,14 +44,17 @@ class Foodmenu extends React.Component{
 
   render()
   {
+    console.log("Render: "+ this.state.data)
     return(
+       
         <div>
         <ul>
-                    {this.state.data && this.state.data.map(function(data, index) {
+                    {this.state.data.map(function(foodItem, index) {
                     return (
                         <div key={index}>
-                                <h1>{this.data.name}</h1>
-                                <p>{this.data.calories}</p>
+                                <h1>{foodItem.id}</h1>
+                                <h1>{foodItem.name}</h1>
+                                <p>{foodItem.calories}</p>
                         </div>
                     )
                     }
@@ -59,4 +65,50 @@ class Foodmenu extends React.Component{
     )
   }
 }
+
+
+/*
+//ajax 
+class Foodmenu extends React.Component {
+    constructor(){
+        super() 
+          this.state = {
+            data: []
+          }
+        
+      }
+      componentDidMount() {
+        $.ajax({
+           url: "http://localhost:9000/foods",
+           type: "GET",
+           dataType: 'json',
+           ContentType: 'application/json',
+           success: function(data) {
+             
+             this.setState({data: data});
+           }.bind(this),
+           error: function(jqXHR) {
+             console.log(jqXHR);
+           }.bind(this)
+        })
+      }
+      render() {
+        
+        return (
+          <table>
+          <tbody>{this.state.data && this.state.data.map(function(item, key) {
+                 
+                   return (
+                      <tr key = {key}>
+                          <td>{item.name}</td>
+                          <td>{item.calorie}</td>
+                      </tr>
+                    )
+                 
+                 })}</tbody>
+           </table>
+        )
+      }
+}
+*/
 export default Foodmenu;
